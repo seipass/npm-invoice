9826351870c3df8ec2cfd5e0c2acdba7618cae00
Hasilan <136697724+seipass@users.noreply.github.com>
2026-09-22 23:37:31 +0900

    feat: export public API

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { analyzePackage } from './analyze.js';
+export { RegistryClient, resolveVersion } from './registry.js';
+export { renderInvoice, formatBytes } from './render.js';
+export { parsePackageSpec, normalizeDependency } from './spec.js';
